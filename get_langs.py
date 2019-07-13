201f3560e58ca8c74bd3cdf9c5349c94c0eabfa2
Luca Marcelli <luca@cerrato.ch>
2019-07-13 12:21:00 +0200

    add select generation script for languages

---
+++ get_langs.py
@@ -0,0 +1,16 @@
+from os import listdir
+from os.path import isfile, join
+import re
+
+PATH = "static/js/"
+REGEX = r"mode-(.+).js"
+files = [f for f in listdir(PATH) if isfile(join(PATH, f))]
+
+langs = []
+for file in files:
+	match = re.search(REGEX, file, re.IGNORECASE)
+	if match:
+		langs.append(match.group(1))
+
+for lang in langs:
+	print "<option value=\"%s\">%s</option>" % (lang, lang)
